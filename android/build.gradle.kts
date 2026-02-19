c20952856401f5436ae3753e9063157a77db4a4f
Abbah77 <yakubuyakson77@gmail.com>
2026-02-20 00:32:23 +0100

    Add files via upload

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+// Root-level build.gradle.kts
+plugins {
+    // 1. Core Android plugin - Required for any Android app
+    id("com.android.application") version "8.7.0" apply false
+    
+    // 2. Kotlin plugin - Required for Flutter's Android side
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    
+    // 3. Google Services plugin - Required for Firebase
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -5,6 +17,10 @@ allprojects {
     }
 }
 
+/**
+ * Customizing build directories: 
+ * This moves the build folder out of the project root to keep the structure clean.
+ */
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -15,6 +31,8 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// Ensures the ':app' project is evaluated first to prevent configuration errors
 subprojects {
     project.evaluationDependsOn(":app")
 }
